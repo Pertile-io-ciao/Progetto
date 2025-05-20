9a87328c395f14877de714d3b61c867cbd3b2f1b
Pertile-io-ciao <chiara.pertile777@gmail.com>
2025-05-20 15:56:23 +0200

    .

--- dati.cpp
+++ dati.cpp
@@ -1,22 +1,25 @@
 #include "dati.hpp"
 #include <cmath>
+#include  <SFML/Graphics.hpp>
 
-int bianconero (pixel p){
+
+
+std::vector<sf::Color> immagineVettore(const sf::Image& image, int height, int width) {
+    std::vector<sf::Color> risultato;
+
+    for (int y = 0; y < height; ++y) {         
+        for (int x = 0; x < width; ++x) {      
+            risultato.push_back(image.getPixel(x,y));
+        }
+    }
+
+    return risultato;
+}
+
+int bianconero (immagineVettore p){
     double norma= std::sqrt(p.r * p.r + p.g * p.g + p.b *p.b );
     if (norma<127){
         return -1;
     }
     return 1;
 }
-
-std::vector<pixel> immagineInVettore(pixel** matrice, int height, int width) {
-    std::vector<pixel> risultato;
-
-    for (int y = 0; y < height; ++y) {         // alto -> basso
-        for (int x = 0; x < width; ++x) {      // sinistra -> destra
-            risultato.push_back(matrice[y][x]);
-        }
-    }
-
-    return risultato;
-}           //chatgpt CHIEDI    prend ei pixel non l'immagine
