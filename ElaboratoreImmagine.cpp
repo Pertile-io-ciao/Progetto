93308c08a7e8becadb5783a7ee79f6cf95feca6a
meggyrizzo <meggy.rizzo@gmail.com>
2025-05-21 08:56:30 +0200

    creazione della classe astratta elaboratore di immmagini

---
+++ ElaboratoreImmagine.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include "ElaboratoreImmagine.hpp"
+
+
+ElaboratoreImmagine::ElaboratoreImmagine(std::string source, std::string destination) {
+    this->sourceFolder = source;
+    this->destinationFolder = destination;
+}
+
+void ElaboratoreImmagine::elabora() {
+    
+    
+    if (!std::filesystem::exists(this->destinationFolder)) {
+        std::filesystem::create_directories(this->destinationFolder);
+    }
+
+    /**
+     * Itero su tutti i file png della cartella sorgente
+     */
+    for (const auto& entry : std::filesystem::directory_iterator(this->sourceFolder)) {
+        /**
+         * Verifico che sia un file 
+         * e che sia png
+         */
+        if (entry.is_regular_file()) {
+            auto path = entry.path();
+            std::string ext = path.extension().string();
+            if (ext == ".png") {
+                sf::Image img;
+                //carico immagine
+                img.loadFromFile(path.string());
+
+                /**
+                 * Chiamo il metodo specifico di trasformazine
+                 */
+                sf::Image elaborata = trasforma(img);
+
+                //salvo risultato in destinationFolder con lo stesso nome
+                std::string outPath = this->destinationFolder + "/" + path.filename().string();
+                elaborata.saveToFile(outPath);
+            }
+        }
+    }
+}
+
+int main() {
+    return 0;
+}
