400787265b9e2fa1f6dcae92ac2c256db6a29c61
Pertile-io-ciao <chiara.pertile777@gmail.com>
2025-05-22 11:12:08 +0200

    cercando di cambiare skin al gatto

--- main.cpp
+++ main.cpp
@@ -6,10 +6,15 @@
 
 
 int main ()  {
-    disegna();
+
     sf::Image image;
     if (!image.loadFromFile("imageKitty.png")) {
         std::cerr << "Errore nel caricamento dell'immagine.\n";
+        return -1;
+    }
+    sf::Texture texture;
+    if (!texture.loadFromFile("imageKitty.png")) {
+        std::cerr << "Errore nel caricamento di imageMiao.png\n";
         return -1;
     }
 
@@ -19,7 +24,16 @@ int main ()  {
     std::vector<sf::Color> colori = immagineVettore(image, 64, 64);
     std::vector<int> vettore= vettore1(colori);
     std::cout << "Vettore di interi:\n";
-    for (std::size_t i = 0; i < vettore.size(); ++i) {
+    for (std::size_t i = 0; i < 14; ++i) {
     std::cout << "Elemento " << i << ": " << vettore[i] << "\n";
-}
+    }
+    auto v=interpolazioneBilineare(vettore, width, height);
+    sf::Image imagebw = vettoreInImmagine(v);
+    sf::Texture texturebw;
+    if (!texturebw.loadFromImage(imagebw)) {
+        std::cerr << "Errore nel caricamento di imageMiao.png\n";
+        return -1;
+    }
+    
+    disegna(texture, texturebw);
 }
