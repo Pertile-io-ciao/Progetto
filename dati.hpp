e7a5ab2cee27e4dc92ee2233e097f58617a8a93d
Pertile-io-ciao <chiara.pertile777@gmail.com>
2025-05-22 11:12:48 +0200

    cercando di cambiare skin al gatto

--- dati.hpp
+++ dati.hpp
@@ -11,5 +11,5 @@ struct pixel
 std::vector<sf::Color> immagineVettore(const sf::Image& image, int height, int width);
 int bianconero (pixel p);
 std::vector<int> vettore1 (const std::vector<sf::Color>& v);
-
-sf::Image interpolazioneBilineare(const sf::Image& src, unsigned int newWidth, unsigned int newHeight);
+std::vector<int> interpolazioneBilineare(const std::vector<int>& input, int inW, int inH);
+sf::Image vettoreInImmagine(const std::vector<int>& dati);
