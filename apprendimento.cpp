bc922207f581bad1114aae89aee5e5b04cad75a4
Pertile-io-ciao <chiara.pertile777@gmail.com>
2025-05-28 12:03:19 +0200

    hebb

--- apprendimento.cpp
+++ apprendimento.cpp
@@ -1,1 +1,6 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
 #include "apprendimento.hpp""
+
+std::vector<int> hebb(const std::vector<int>& v, int p, int n){
+}
--- apprendimento.hpp
+++ apprendimento.hpp
@@ -1,3 +1,3 @@
+#include <SFML/Graphics.hpp>
 #include <vector>
-
-std::vector<int> hebb(const std::vector<int>& v);
+std::vector<int> hebb(const std::vector<int>& v, int p, int n);
